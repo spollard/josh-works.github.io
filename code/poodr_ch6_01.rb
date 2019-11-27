49ddd43c9243784d1c4fe2f99d6179657e290a5f
Josh Thompson <thompsonjoshd@gmail.com>
2019-11-27 02:08:42 +0200

    pre-abstrac superclass Bicycle

--- code/poodr_ch6_01.rb
+++ code/poodr_ch6_01.rb
@@ -1,40 +1,52 @@
-class Bicyle
-  attr_reader :style, :size, :tape_color, :front_shock, :rear_shock
+class Bicycle
+end
+
+
+class MountainBike < Bicycle
+  attr_reader :size, :tape_color, :front_shock, :rear_shock
   def initialize(args)
-    @style = args[:style]
-    @size = args[:size]
-    @tape_color = args[:tape_color]
-    @front_shock = args[:front_shock]
-    @rear_shock = args[:rear_shock]
+    @size         = args[:size]
+    @tape_color   = args[:tape_color]
+    @front_shock  = args[:front_shock]
+    @rear_shock   = args[:rear_shock]
   end
   
   def spares
-    { 
-      chain: '10-speed',
-      tire_size: '23',
-      tape_color: tape_color
+    { chain:      '10-speed',
+      tire_size:  '2.1',
+      rear_shock: rear_shock
     }
   end
 end
 
-class MountainBike < Bicyle
-  attr_reader :front_shock, :rear_shock
+class RoadBike < Bicycle
+  attr_reader :size, :tape_color
   def initialize(args)
-    @front_shock = args[:front_shock]
-    @rear_shock = args[:rear_shock]
-    super(args)
+    @size       = args[:size]
+    @tape_color = args[:tape_color]
   end
   
   def spares
-    super.merge(rear_shock: rear_shock)
+    { chain:      '10-speed',
+      tire_size:  '23',
+      tape_color: tape_color }
   end
 end
 
+
 mtb = MountainBike.new(
-  style: :mountain,
-  size: 'S',
-  front_shock: 'Manitou',
-  rear_shock: 'Fox'
-  )
-require "pry"; binding.pry
+  front_shock: 'manitou',
+  rear_shock: 'fox'
+)
+
+road = RoadBike.new(
+  tape_color: "Red",
+)
 p mtb
+p mtb.spares
+
+p road
+p road.spares
+
+require "pry"; binding.pry
+
