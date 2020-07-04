4355818925d4024f8e5aa1ddab40c9489fb9c613
Josh Thompson <thompsonjoshd@gmail.com>
2020-07-04 08:02:47 -0400

    First MVP! styles update, event listeners work, etc

--- incremental-css/script.js
+++ incremental-css/script.js
@@ -1,28 +1,41 @@
 document.getElementById("startOver").addEventListener("click", startOver); 
-document.getElementById("showContent").addEventListener("click", revealContent); 
+
+var html = document.querySelector('html');
+var stepLinks = document.getElementsByClassName('step')
+var sections = document.querySelectorAll('section')
+
+for (var i = 0; i < stepLinks.length; i++) {
+  console.log("adding event listeners");
+  stepLinks[i].addEventListener("click", nextStep)
+}
+
+
+function nextStep() {
+  var currentStep = this.dataset.step
+  console.log(currentStep);
+  var nextStep = parseInt(currentStep) + 1
+  sections[nextStep].classList.remove("hidden")
+  html.classList.add("step" + nextStep.toString())
+}
+
+var firstSection = sections[0]
 
 function startOver() {
-  var sections = document.getElementsByTagName("section");
+  html.className = ''
   for (var i = 0; i < sections.length; i++) {
     sections[i].classList.add("hidden")
   }
-  var start = document.getElementById("start");
-  start.classList.remove("hidden")
-
+  firstSection.classList.remove("hidden")
 }
  
 function revealContent() {
-  reveal();
+  console.log("trying to reveal");
+  html.classList.add("step0")
+  sections[1].classList.remove("hidden")
 } 
 
-function hideAll() {
-  var element = document.getElementById("start");
-  element.classList.add("hidden");
-}
-
-function reveal() {
-  console.log("trying to reveal");
-  var element = document.getElementById("content");
-  element.classList.remove("hidden");
-  
-}
+function revealNextSection() {
+  console.log("trying to reveal text-styling");
+  html.classList.add("step1")
+  sections[2].classList.remove("hidden")
+} 
